e18236049023864fe95b4ee455493564d23d4bdf
Arohi Mehta <114707955+aumehta@users.noreply.github.com>
2024-01-06 00:44:25 -0500

    Add files via upload

---
+++ IoTMachineLearningPart1.py
@@ -0,0 +1,202 @@
+﻿
+import pandas as pd
+import sklearn
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn import svm
+from sklearn.feature_selection import VarianceThreshold
+
+from scipy.interpolate import interp1d
+
+df = pd.read_csv('four_LETbulbs_with_timestamps.csv')
+ 
+ 
+def lightbulbName(originalName):
+     if(lightbulb == '78:6d:eb:b2:da:d3'):
+        return 'd3'
+     elif(lightbulb == '78:6d:eb:48:44:b8'):
+        return 'b8'
+     elif(lightbulb == '78:6d:eb:48:45:64'):
+        return '64'
+     elif(lightbulb == '78:6d:eb:b2:2b:ac'):
+        return 'ac'
+ 
+ 
+def sortList(list):
+  returnList = []
+  indexAC = list.index('ac')
+  returnList.append(indexAC)
+  indexB8 = list.index('b8')
+  returnList.append(indexB8)
+  indexd3 = list.index('d3')
+  returnList.append(indexd3)
+  index64 = list.index('64')
+  returnList.append(index64)
+  return returnList
+ 
+timeOriginal = df.iloc[:,3][0] #first time in dataframe
+listTotalBulb = []
+bulbOriginal = df.iloc[:,0][0] #first time in dataframe
+listAppendBulb = []
+listTotalOutput = []
+listTotalStrength = []
+listAppendStrength = []
+listTotalTime = [] 
+listAppendTime = [] 
+signalOriginal = df.iloc[:,1][0]
+print(signalOriginal)
+indexCount= -1
+ 
+for index, row in df[0:].iterrows():
+  lightbulb = row[0]
+  strength = row[1]
+  time = row[3]
+  output = row[2]
+  if(time - timeOriginal <=6):
+     shortBulbName = lightbulbName(lightbulb)
+     indexCount = indexCount +1
+     if(indexCount>0):
+        if shortBulbName in listAppendBulb:
+           index = listAppendBulb.index(shortBulbName)
+           previousStrength = listAppendStrength[index]
+           averageStrength =(previousStrength + strength)/2
+           listAppendStrength[index] = np.round(averageStrength,3)
+ 
+        else:
+           listAppendBulb.append(shortBulbName)
+           listAppendStrength.append(strength)
+           listAppendTime.append(time)
+ 
+     else:
+        listAppendBulb.append(shortBulbName)
+        listAppendStrength.append(strength)
+        listAppendTime.append(time)
+     
+  else:
+     counter = 0
+     index  = 0
+     list = listAppendBulb
+     for elem in list:
+        if('ac' not in list):
+           list.append('ac')
+           counter = counter +1
+        if('b8' not in list):
+           list.append('b8')
+           counter = counter +1
+        if('d3' not in list):
+           list.append('d3')
+           counter = counter+1
+        if('64' not in list):
+           list.append('64')
+           counter  = counter +1
+     while(index < counter):
+        listAppendStrength.append(0)
+        index = index+1
+     changeIndexStrength= sortList(listAppendBulb)
+     newListAppendBulb = []
+     newListAppendStrength = []
+     for index in changeIndexStrength:
+        newListAppendBulb.append(listAppendBulb[index])
+        newListAppendStrength.append(listAppendStrength[index])
+     listTotalBulb.append(newListAppendBulb)
+     listTotalStrength.append(newListAppendStrength)
+     listTotalTime.append(listAppendTime)
+     listTotalOutput.append(output)
+     timeOriginal = time
+     listAppendBulb = []
+     listAppendStrength = []
+     listAppendTime = [] 
+     indexCount = 0
+     listAppendBulb.append(lightbulbName(lightbulb))
+     listAppendStrength.append(strength)
+     listAppendTime.append(time)
+ 
+ 
+# from sklearn import datasets
+ 
+ 
+# import pandas as pd
+ 
+ 
+# Import train_test_split function
+from sklearn.model_selection import train_test_split
+ 
+# Split dataset into training set and test set
+X_train, X_test, y_train, y_test = train_test_split(listTotalStrength, listTotalOutput, test_size=0.3,random_state=109) # 70% training and 30% test
+ 
+from sklearn import svm
+ 
+#Create a svm Classifier
+clf = svm.SVC(kernel='linear') # Linear Kernel
+ 
+clf.fit(X_train, y_train)
+#Predict the response for test dataset
+y_pred = clf.predict(X_test)
+ 
+#Import scikit-learn metrics module for accuracy calculation
+from sklearn import metrics
+ 
+# Model Accuracy: how often is the classifier correct?
+print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
+print("Precision: " , sklearn.metrics.precision_score(y_test, y_pred, average = "weighted"))
+
+ 
+#take output and input data in a different file
+ 
+ 
+# from sklearn import preprocessing, svm
+# from sklearn.model_selection import train_test_split
+# from sklearn.linear_model import LinearRegression
+ # # Dropping any rows with Nan values
+# X_train, X_test, y_train, y_test = train_test_split(dfStrength, listTotalOutput, test_size = 0.25)
+ # # Splitting the data into training and testing data
+# regr = LinearRegression()
+ # regr.fit(X_train, y_train)
+# print(regr.score(X_test, y_test))
+ 
+from sklearn import tree
+from sklearn.model_selection import train_test_split # Import train_test_split function
+from sklearn.tree import DecisionTreeClassifier # Import Decision Tree Classifier
+X_train, X_test, y_train, y_test = train_test_split(listTotalStrength, listTotalOutput, test_size=0.3, random_state=1) # 70% training and 30% test
+clf = DecisionTreeClassifier()
+ 
+# Train Decision Tree Classifer
+clf = clf.fit(X_train,y_train)
+ 
+#Predict the response for test dataset
+y_pred = clf.predict(X_test)
+from sklearn import metrics #Import scikit-learn metrics module for accuracy calculation
+ 
+print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
+
+clf = DecisionTreeClassifier(criterion="entropy", max_depth=12)
+ 
+# Train Decision Tree Classifer
+clf = clf.fit(X_train,y_train)
+ 
+#Predict the response for test dataset
+y_pred = clf.predict(X_test)
+ 
+# Model Accuracy, how often is the classifier correct?
+print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
+print("Precision: " , sklearn.metrics.precision_score(y_test, y_pred, average = "weighted"))
+
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(listTotalStrength, listTotalOutput, test_size=0.25, random_state=0)
+from sklearn.linear_model import LogisticRegression
+logisticRegr = LogisticRegression()
+logisticRegr.fit(x_train, y_train)
+predictions = logisticRegr.predict(x_test)
+score = logisticRegr.score(x_test, y_test)
+print(score)
+ 
+#looks at models that can change the input width
+#r and n can be used to change the arrival length
+#load data from external file
+# use data interprolation
+
+
+
+
+
---
+++ IoTMachineLearningPart2.py
@@ -0,0 +1,221 @@
+﻿
+import pandas as pd
+import sklearn
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn import svm
+from sklearn.feature_selection import VarianceThreshold
+import math
+from scipy.interpolate import interp1d
+from collections import Counter
+from sklearn.model_selection import train_test_split
+
+
+df = pd.read_csv('four_LETbulbs_with_timestamps.csv')
+ 
+ 
+def lightbulbName(originalName):
+     if(originalName == '78:6d:eb:b2:da:d3'):
+        return 'd3'
+     elif(originalName == '78:6d:eb:48:44:b8'):
+        return 'b8'
+     elif(originalName == '78:6d:eb:48:45:64'):
+        return '64'
+     elif(originalName == '78:6d:eb:b2:2b:ac'):
+        return 'ac'
+ 
+ 
+def sortList(list):
+  returnList = []
+  indexAC = list.index('ac')
+  returnList.append(indexAC)
+  indexB8 = list.index('b8')
+  returnList.append(indexB8)
+  indexd3 = list.index('d3')
+  returnList.append(indexd3)
+  index64 = list.index('64')
+  returnList.append(index64)
+  return returnList
+ 
+timeAC = [] 
+strengthAC = [] 
+outputAC = []
+timeB8 = [] 
+strengthB8= [] 
+outputB8 = [] 
+timeD3 = []
+strengthD3 = []  
+outputD3 = [] 
+time64 = []
+strength64 = [] 
+output64 = [] 
+totalStrength = [] 
+timeX = []
+
+
+
+def checkBulbs(): 
+   for index, row in df[0:].iterrows():
+      bulb = row[0] #first time in dataframe
+      output = row[2]
+      bulbName = lightbulbName(bulb)
+      timeX.append(row[3])
+      if(bulbName == 'ac'):
+         timeAC.append(row[3])
+         strengthAC.append(row[1])
+         outputAC.append(output)
+      if(bulbName == 'b8'):
+         timeB8.append(row[3])
+         strengthB8.append(row[1])
+         outputB8.append(output)
+      if(bulbName == 'd3'):
+         timeD3.append(row[3])
+         strengthD3.append(row[1])
+         outputD3.append(output)
+      if(bulbName == '64'):
+         time64.append(row[3])
+         strength64.append(row[1])
+         output64.append(output)
+
+
+checkBulbs() 
+
+bulbac = interp1d(timeAC, strengthAC, kind='nearest', bounds_error=False, fill_value= "extrapolate")
+bulbb8 =  interp1d(timeB8, strengthB8, kind='nearest', bounds_error=False, fill_value= "extrapolate")
+bulbd3 =  interp1d(timeD3, strengthD3, kind='nearest', bounds_error=False, fill_value= "extrapolate")
+bulb64 =  interp1d(time64, strength64, kind='nearest', bounds_error=False, fill_value= "extrapolate")
+
+outputACIP = interp1d(timeAC, outputAC, kind='nearest', bounds_error=False)
+outputB8IP = interp1d(timeB8, outputB8, kind='nearest', bounds_error=False)
+outputD3IP = interp1d(timeD3, outputD3, kind='nearest', bounds_error=False)
+output64IP = interp1d(time64, output64, kind='nearest', bounds_error=False)
+outputACFinal= outputACIP(timeX).tolist()
+outputB8Final= outputB8IP(timeX).tolist()
+outputD3Final= outputD3IP(timeX).tolist()
+output64Final= output64IP(timeX).tolist()
+
+listReturnOutput = []
+def cleanOutputData(): 
+   for i in range(37643):
+      valueAC = outputACFinal[i]
+      valueB8 = outputB8Final[i]
+      valueD3 = outputD3Final[i]
+      value64 = output64Final[i]
+      countNan = 0
+      if(math.isnan(valueAC)):
+         countNan = countNan +1
+      elif(math.isnan(valueB8)):
+         countNan = countNan +1
+      elif(math.isnan(valueD3)):
+         countNan = countNan +1
+      elif(math.isnan(value64)):
+         countNan = countNan +1
+      if(countNan>=2):
+         outputACFinal.pop(i)
+         outputB8Final.pop(i)
+         outputD3Final.pop(i)
+         output64Final.pop(i)
+         timeX.pop(i)
+      else:
+         listVals = [valueAC, valueB8, valueD3, value64]
+         counter = Counter(listVals)
+         most_common = counter.most_common(1)[0][0]
+         listReturnOutput.append(most_common)
+   return listReturnOutput
+
+output = cleanOutputData()
+output[0] = output[1]
+fAC =bulbac(timeX).tolist()
+fB8 = bulbb8(timeX).tolist()
+fD3 = bulbd3(timeX).tolist()
+f64 = bulb64(timeX).tolist()
+
+listTotalVectors = [] 
+for x in range(37643):
+   listAppend = [] 
+   listAppend.append(fAC[x])
+   listAppend.append(fB8[x])
+   listAppend.append(fD3[x])
+   listAppend.append(f64[x])
+   listTotalVectors.append(listAppend)
+print(timeX[0:5])
+print(listTotalVectors[0:5])
+
+# X_train, X_test, y_train, y_test = train_test_split(listTotalVectors, output, test_size=0.3) # 70% training and 30% test
+# from sklearn import svm
+
+# #Create a svm Classifier
+# clf = svm.SVC() #
+
+# #Train the model using the training sets
+# clf.fit(X_train, y_train)
+
+# #Predict the response for test dataset
+# y_pred = clf.predict(X_test)
+# from sklearn import metrics
+
+# # Model Accuracy: how often is the classifier correct?
+# print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
+# print("Precision: " , sklearn.metrics.precision_score(y_test, y_pred, average="weighted"))
+# print("F1 score: " , sklearn.metrics.f1_score(y_test, y_pred, average = "weighted"))
+
+# from sklearn import preprocessing, svm
+# from sklearn.model_selection import train_test_split
+# from sklearn.linear_model import LinearRegression
+#  # Dropping any rows with Nan values
+# X_train, X_test, y_train, y_test = train_test_split(listTotalVectors, output, test_size = 0.25)
+#  # Splitting the data into training and testing data
+# regr = LinearRegression()
+# regr.fit(X_train, y_train)
+# print(regr.score(X_test, y_test))
+ 
+# from sklearn import tree
+# from sklearn.model_selection import train_test_split # Import train_test_split function
+# from sklearn.tree import DecisionTreeClassifier # Import Decision Tree Classifier
+# X_train, X_test, y_train, y_test = train_test_split(listTotalVectors,  output, test_size=0.3, random_state=1) # 70% training and 30% test
+# clf = DecisionTreeClassifier()
+ 
+# # Train Decision Tree Classifer
+# clf = clf.fit(X_train,y_train)
+ 
+# #Predict the response for test dataset
+# y_pred = clf.predict(X_test)
+# from sklearn import metrics #Import scikit-learn metrics module for accuracy calculation
+ 
+# print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
+# print("Precision: " , sklearn.metrics.precision_score(y_test, y_pred, average="weighted"))
+# print("F1 score: " , sklearn.metrics.f1_score(y_test, y_pred, average = "weighted"))
+# clf = DecisionTreeClassifier(criterion="entropy", max_depth=12)
+ 
+# # Train Decision Tree Classifer
+# clf = clf.fit(X_train,y_train)
+ 
+# #Predict the response for test dataset
+# y_pred = clf.predict(X_test)
+ 
+# # Model Accuracy, how often is the classifier correct?
+# print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
+# print("Precision: " , sklearn.metrics.precision_score(y_test, y_pred, average = "weighted"))
+# print("F1 score: " , sklearn.metrics.f1_score(y_test, y_pred, average = "weighted"))
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(listTotalVectors, output, test_size=0.25, random_state=0)
+from sklearn.linear_model import LogisticRegression
+logisticRegr = LogisticRegression()
+logisticRegr.fit(x_train, y_train)
+predictions = logisticRegr.predict(x_test)
+score = logisticRegr.score(x_test, y_test)
+print(score)
+from sklearn import metrics
+
+# Model Accuracy: how often is the classifier correct?
+
+ 
+#can we add more lightbulbs and will that increase accuracy? 
+# if we only have two or three lightbulbs, if that decreases then we can add lightbulbs 
+# if we only have two lightbublbs, how will the models perfrom 
+#manipulate input data 
+#try 3 lightbulbs after 
+#precision, f1 scores, basic metrics 
+#summarize results in spreadsheet 
+#easier to manage data , recall, precision 
